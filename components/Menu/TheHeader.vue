ce8187ee1016a9509a987362d5f0676d4b4482ec
lottehof <s1112052@student.hsleiden.nl>
2021-02-08 14:29:34 +0100

    first

---
+++ components/Menu/TheHeader.vue
@@ -0,0 +1,114 @@
+<template>
+    <header>
+      <div class="drawer-toggle" role="button" @click="$store.dispatch('nav/toggleSidebar')">
+          <div class="bar"></div>
+          <div class="bar"></div>
+      </div>
+
+        <div class="brand-name" @click="$store.dispatch('nav/toggleSidebar')">
+            <nuxt-link to="/">
+              Menu
+            </nuxt-link>
+        </div>
+
+
+
+    </header>
+</template>
+
+<script>
+    import AppLinks from '~/components/Menu/AppLinks'
+    export default {
+        components: { AppLinks }
+    }
+</script>
+
+<style scoped>
+
+    header{
+      position: absolute;
+      display: flex;
+      /* grid-template: 90px / auto 1fr 1fr; */
+      align-items: center;
+      background-color: transparent;
+      background-position: top;
+      padding: 0 16px;
+      height: 100px;
+      width: 100vw;
+        /* background-image: url("~assets/img/bg2.jpg");
+        background-position: center;
+        background-size: cover; */
+
+    }
+    header:nth-child{
+        justify-self: end !important;
+    }
+    .app-links{
+        justify-self: end;
+        display: none;
+    }
+    .brand-name {
+        margin: 0px;
+        font-size: 18px;
+        margin-right: 20px;
+        /* width: 200px; */
+         font-family: HelveticaNeue;
+    }
+
+
+    .brand-name a {
+        text-decoration: none;
+        color: white;
+    }
+    .drawer-toggle .bar {
+        width: 70%;
+        height: 2px;
+        background-color: white;
+    }
+    .drawer-toggle {
+        display: flex;
+        justify-self: start;
+        flex-direction: column;
+        justify-content: space-around;
+        height: 20%;
+        width: 25px;
+        cursor: pointer;
+    }
+
+
+    @media (min-width: 430px) {
+      .logo{
+        width: 40%;
+        margin-right: 30px;
+        height: auto;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+    }
+
+    @media (min-width: 900px) {
+      .logo{
+        width: 300px;
+        margin-right: 30px;
+        height: auto;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      .drawer-toggle{
+        margin-left: 35px;
+      }
+
+    }
+
+
+    /* @media (min-width: 768px) {
+        header{
+            padding: 0 64px;
+        }
+        .drawer-toggle {
+            display: block;
+            height: 30%;
+        }
+    } */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template lang="html">
+  <div class="total">
+    <div class="start">
+      <div class="test">
+          <img src="@/assets/img/Logo2.png" alt="logo" class="logo">
+
+        </div>
+
+    </div>
+    <History />
+    <AluSloepen />
+    <AluSportboot />
+    <Information />
+
+
+
+    <Instagram />
+    <FooterMenu />
+    <Footer />
+
+
+
+
+
+      </div>
+
+</template>
+
+<script>
+import History from '@/components/History/History'
+import AluSloepen from '@/components/Modellen/AluSloepen'
+import AluSportboot from '@/components/Modellen/AluSportboot'
+import Instagram from '@/components/Instagram/Instagram'
+import FooterMenu from '@/components/Footer/FooterMenu'
+import Footer from '@/components/Footer/Footer'
+import Information from '@/components/Information/Information'
+export default {
+  components: {
+    History,
+    AluSloepen,
+    Instagram,
+    AluSportboot,
+    FooterMenu,
+    Footer,
+    Information,
+  }
+
+}
+</script>
+<style lang="css" scoped>
+.logo{
+  position: absolute;
+  top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 150px;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.total{
+  width: 100vw;
+}
+.start{
+  /* position: absolute; */
+  width: 100vw;
+  height: 879px;
+  /* top:0; */
+  background-image: url("~assets/img/bg2.jpg");
+  background-position: center;
+  background-size: cover;
+}
+.test{
+  width: 100vw;
+  height: 879px;
+  /* position: absolute;
+  top: 0; */
+  background-image: linear-gradient(to bottom, #000000 -7%, rgba(3, 3, 3, 0.99) -6%, rgba(216, 216, 216, 0));
+}
+
+@media (min-width: 600px) {
+  .logo{
+    top: 40px;
+    width: 350px;
+
+  }
+}
+
+
+</style>
